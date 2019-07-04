55ae0a13a8852b5cbaf494fd9f9f406afae95777
cloudymous <sayacloud@gmail.com>
2019-07-04 16:07:11 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<<?php
+  include('login.php'); // Includes Login Script
+
+  if(isset($_SESSION['login_user'])){
+    header("location: profile.php");
+  }
+?>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  </head>
+  <body>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        <?php
+          echo "Login Succes";
+        ?>
+      </h1>
+    </div>
+  </section>
+  </body>
+</html>
