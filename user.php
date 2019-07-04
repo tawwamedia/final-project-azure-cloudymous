77fe3d22e22c2d42f155aefe0c97a079e0b4da4b
cloudymous <sayacloud@gmail.com>
2019-07-04 17:13:32 +0800

    Move connection php

--- user.php
+++ user.php
@@ -53,7 +53,14 @@
 
   <<?php
 
-    include 'connection.php';
+  try {
+      $conn = new PDO("sqlsrv:server = tcp:cloudymousappserv.database.windows.net,1433; Database = dicodingdb", "cloudymous", "imran*01");
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $e) {
+      print("Error connecting to SQL Server.");
+      die(print_r($e));
+  }
 
     if (isset($_POST['adduser'])) {
   		try {
