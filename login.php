22c70ca29475b464df6ee156d8479e82dd474239
cloudymous <sayacloud@gmail.com>
2019-07-09 08:51:07 +0800

    Coba kasih center

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
       <div class="columns is-centered">
         <div class="column is-half">
           <div class="container">
-            <h1 class="title">Please Login</h1>
+            <h1 class="title is-centered">Please Login</h1>
             <div class="field">
               <p class="control has-icons-left has-icons-right">
                 <input class="input" type="email" placeholder="Email">
