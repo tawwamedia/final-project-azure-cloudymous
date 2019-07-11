0c849b87a411bd7884690c7adce6786d00edb975
cloudymous <sayacloud@gmail.com>
2019-07-11 10:14:44 +0800

    I dont know what to do

---
+++ database/config.php
@@ -0,0 +1,8 @@
+<?php
+
+  define('DB_SERVER', "tcp:cloudymousappserv.database.windows.net,1433");
+  define('DB_USER', "cloudymous");
+  define('DB_PASS', "imran*01");
+  define('DB_DATABASE', "dicodingdb")
+
+?>
